4fa4faccf88c80240214f37ad504e256bda3c742
JOSE RIBEIRO <jribeiro@ufu.br>
2025-07-06 17:04:33 -0300

    correção no endereço (manifest e outros)

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
 self.addEventListener('install', event => {
   console.log('Service Worker: Installed');
+  event.waitUntil(self.skipWaiting()); // força ativação imediata
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim()); // pega controle das páginas atuais
 });
